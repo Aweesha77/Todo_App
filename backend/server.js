5678e0f3e0d7c18dd921aa5ff892a8787c16697e
Kashuni Aweesha Kodithuwakku <126492783+Aweesha77@users.noreply.github.com>
2024-05-06 01:18:54 +0530

    done with creating model and routes

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,10 @@ const app=express();
 const PORT=process.env.port || 8000;
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)})
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{console.log("Connected to database")})
+.catch((err)=>{console.log(err)})
+
+const routes=require('./routes/TodoRoute')
+app.use(routes);
